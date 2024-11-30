4814945a4dba73bffb2c8134a724ed2cbf3e21e8
Vyom Galia <103774869+Vyomgalia@users.noreply.github.com>
2024-11-30 10:51:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,6 @@ def main():
             </style>
             """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
     # Custom CSS for styling
     st.markdown("""
