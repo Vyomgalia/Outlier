4efce912cb466d1cd1b90699889827a3c8114191
Vyom Galia <103774869+Vyomgalia@users.noreply.github.com>
2024-12-03 17:26:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,9 +93,13 @@ def main():
         footer:has([title="Streamlit badge"]) {
             display: none !important;
         }
+        button[data-testid="manage-app-button"] {
+            display: none !important;
+        }
         </style>
     """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 
 
     # Custom CSS for styling
