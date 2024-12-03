0cb7f44950e1cdc9dec09511e6400bd5a61a65de
Vyom Galia <103774869+Vyomgalia@users.noreply.github.com>
2024-12-03 16:53:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -639,10 +639,10 @@ def main():
 
     # Instagram Follow Section
     st.markdown("""
-        <a href="https://replygeneratorpy.streamlit.app/" target="_blank" class="instagram-card" id="instagram-card" style="text-decoration: none; color: white;">
+        <a href="https://www.instagram.com/outliercrew?igsh=MXZweHk5Yzl0bWhzZg==" target="_blank" class="instagram-card" id="instagram-card" style="text-decoration: none; color: white;">
             <p class="heading2" style="font-family: 'Arial Black', Gadget, sans-serif; text-decoration: none; color: white;">CLICK HERE</p>
-            <p style="color: white;">to unlock the</p>
-            <p style="font-size: 20px; font-weight: 900; font-family: 'Arial Black', Gadget, sans-serif; text-decoration: none; color: white;"><strong>Reply Generator AI Tool!</strong></p>
+            <p style="color: white;">to follow us on</p>
+            <p style="font-size: 20px; font-weight: 900; font-family: 'Arial Black', Gadget, sans-serif; text-decoration: none; color: white;"><strong>Instagram</strong></p>
         </a>
     """, unsafe_allow_html=True)
 
